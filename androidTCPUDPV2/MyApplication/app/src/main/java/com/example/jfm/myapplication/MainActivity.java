041128e95c47a8c199f7ff75c1423598ac96141f
jfmolano1587 <jose.francisco.molano@gmail.com>
2015-07-10 17:48:37 -0500

    Codigo de test para medir ruido
    Ya se tiene el texto variable en el tiempo

--- androidTCPUDPV2/MyApplication/app/src/main/java/com/example/jfm/myapplication/MainActivity.java
+++ androidTCPUDPV2/MyApplication/app/src/main/java/com/example/jfm/myapplication/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.jfm.myapplication;
 
 import android.app.Activity;
+import android.media.MediaRecorder;
 import android.net.wifi.WifiInfo;
 import android.net.DhcpInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.format.Formatter;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.location.Location;
 import android.content.Context;
@@ -18,10 +22,22 @@ import android.text.format.Formatter;
 public class MainActivity extends Activity {
 
     private Button btnInicio;
+    private TextView textoRuido;
     private LocationManager locationManager;
     private WifiManager wifiMgr;
     private WifiInfo wifiInfo;
     private DhcpInfo dhcpInfo;
+    private MediaRecorder mRecorder = null;
+    private Handler handler;
+    private int num;
+    Runnable runnable=new Runnable(){
+        @Override
+        public void run() {
+            textoRuido.setText("num "+num);
+            num++;
+            handler.postDelayed(runnable, 250);
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,11 +50,16 @@ public class MainActivity extends Activity {
         //String ipAddress = Formatter.formatIpAddress(ip);
         //Informacion WIFI
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        textoRuido = (TextView) findViewById(R.id.textSonido);
+        handler=new Handler();
+        handler.post(runnable);
+        num = 0;
         addListenerOnButton();
     }
 
     public void medicion()
     {
+        //RUIDO
         //WIFI
         wifiInfo = wifiMgr.getConnectionInfo();
         dhcpInfo = wifiMgr.getDhcpInfo();
@@ -100,6 +121,19 @@ public class MainActivity extends Activity {
                 System.out.println(latitud);
             }
         }
+
+        //while(true)
+        //{
+        //try {
+        //    TextView tv1 = (TextView) findViewById(R.id.textSonido);
+        //    tv1.setText("Hello");
+        //}
+        //catch(Exception e)
+        //{
+        //    System.out.println("- - - - - - - Error - - - - - - - - -");
+        //    Log.i("RM", "error", e);
+        //}
+        //}
     }
 
     public String intToIp(int IpAddress) {
@@ -132,6 +166,37 @@ public class MainActivity extends Activity {
             }
 
         });
+
+    }
+
+    public void start() {
+        try{
+            if (mRecorder == null) {
+                mRecorder = new MediaRecorder();
+                mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                mRecorder.setOutputFile("/dev/null");
+                mRecorder.prepare();
+                mRecorder.start();
+            }
+        }catch(Exception e){}
+
+    }
+
+    public void stop() {
+        if (mRecorder != null) {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+    public double getAmplitude() {
+        if (mRecorder != null)
+            return  mRecorder.getMaxAmplitude();
+        else
+            return 0;
 
     }
 }
